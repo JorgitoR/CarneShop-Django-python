d17d7971ede4945b9a492feeb60fdc2218744750
JorgitoR <jorgitouribe133@gmail.com>
2021-06-11 10:43:42 -0500

    CarneShop Django

--- analitycs/models.py
+++ analitycs/models.py
@@ -13,4 +13,16 @@ class SessionUsuario(models.Model):
 	pais = models.CharField(max_length=120, null=True, blank=True)
 	activo = models.BooleanField(default=True)
 	date = models.DateTimeField(auto_now_add=True)
-	
+
+
+	def __str__(self):
+		ciudad = self.ciudad 
+		pais = self.pais
+		if pais and ciudad:
+			return f"{ciudad}, {pais}"
+		elif ciudad and not pais:
+			return f"{ciudad}"
+		elif pais and not ciudad:
+			return f"{pais}"
+		return self.usuario.username
+		
