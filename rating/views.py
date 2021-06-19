dda05931c1eee8be610c593016211af4e7261772
JorgitoR <jorgitouribe133@gmail.com>
2021-06-19 14:26:08 -0500

    CarneShop Django

--- rating/views.py
+++ rating/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType 
 from django.http import HttpResponseRedirect
+
+
+from .forms import RatingForm
+from .models import rating
+
+
+def RatingView(request):
+	if not request.user.is_authenticated:
+		return HttpResponseRedirect('/')
+	if request.method == 'POST':
+		form = RatingForm(request.POST or None)
+		if form.is_valid():
+			puntaje = form.cleaned_data.get('rating')
+			content_type = form.cleaned_data.get('content_type_id')
+			object_id = form.cleaned_data.get('object_id')
