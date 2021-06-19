debcc6900d8eaaa2d454469e8de38a31c07f2f48
JorgitoR <jorgitouribe133@gmail.com>
2021-06-19 13:09:50 -0500

    CarneShop Django

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,22 +1,39 @@
 from django.contrib import admin
 
-from .models import Usuario, categoria, OrdenarProducto, Orden, pedidos, Cupon, producto, direccion
+from .models import (
+					Usuario, 
+					categoria, 
+					OrdenarProducto, 
+					Orden, 
+					pedidos, 
+					Cupon, 
+					producto, 
+					direccion,
+					ProductoRelacionado)
 
 
 class ContentTypeInline(admin.TabularInline):
 	model = OrdenarProducto
 	extra = 0
 
+class ProductoRelacionadoInline(admin.TabularInline):
+	model = ProductoRelacionado
+	fk_name = 'item'
+	extra = 0
 
 class productoAdmin(admin.ModelAdmin):
 
-	inlines = [ContentTypeInline]
+	inlines = [ProductoRelacionadoInline]
+	class Meta:
+		model = producto
+
+
 
 admin.site.register(direccion)
 admin.site.register(Cupon)
 admin.site.register(pedidos)
 admin.site.register(OrdenarProducto)
 admin.site.register(Orden)
-admin.site.register(producto)
+admin.site.register(producto, productoAdmin)
 admin.site.register(categoria)
 admin.site.register(Usuario)
