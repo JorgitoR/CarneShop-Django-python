bcc39ab6931dd3f78a4351c9e71603a3ac9d743a
JorgitoR <jorgitouribe133@gmail.com>
2021-06-19 14:02:13 -0500

    CarneShop Django

--- rating/models.py
+++ rating/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+from Dashboard.models import Usuario
+from django.contrib.contenttypes.models import ContentType 
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.db.models import Avg
+from django.db.models.signals import pre_save, post_save 
+
+class ClasificacionChoice(models.IntegerChoices):
+	UNO = 1
+	DOS = 2
+	TRES = 3
+	CUATRO = 4
+	CINCO = 5
+
+	__empty__ = 'Clasifica el servicio'
+
+
+class rating(models.Model):
+	usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+	puntaje = models.IntegerField(null=True, blank=True, choiices=ClasificacionChoice.choices)
+	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+	object_id = models.PositiveIntegerField()
+	content_object = GenericForeignKey("content_type", "object_id")
