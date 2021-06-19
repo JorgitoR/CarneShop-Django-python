9469356ef7467ea937989ac0c4a5e1a76e0e490f
JorgitoR <jorgitouribe133@gmail.com>
2021-06-19 14:05:03 -0500

    CarneShop Django

--- rating/models.py
+++ rating/models.py
@@ -16,9 +16,19 @@ class ClasificacionChoice(models.IntegerChoices):
 	__empty__ = 'Clasifica el servicio'
 
 
+class RatingQueryset(models.QuerySet):
+	def rating(self):
+		return self.aggregate(promedio=Avg("puntaje"))["puntaje"]
+
+class RatingManager(models.Manager):
+	def get_queryset(self):
+		return RatingQueryset(self.model, using=self._db)
+
+
 class rating(models.Model):
 	usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 	puntaje = models.IntegerField(null=True, blank=True, choiices=ClasificacionChoice.choices)
 	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
 	object_id = models.PositiveIntegerField()
 	content_object = GenericForeignKey("content_type", "object_id")
+
