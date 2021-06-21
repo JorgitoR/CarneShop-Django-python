1c91d87d62aeeba3539668f95a465258fb93aed6
JorgitoR <jorgitouribe133@gmail.com>
2021-06-21 09:20:54 -0500

    CarneShop Django

--- notificacion/models.py
+++ notificacion/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+from Dashboard.models import Usuario
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+class Notificacion(models.Model):
+	class NIVELES(models.TextChoices):
+		exito = 'Exito', 'exito',
+		info = 'Informacion', 'informacion',
+		advertencia = 'Advertencia', 'advertencia',
+		error = 'Error', 'error'
+
+	nivel = models.CharField(choices=NIVELES.choices, default=NIVELES.info, max_length=20)
+
+	destinario = models.ForeignKey(Usuario, on_delete=models.CASCADE, 
+					related_name='notificar_destino', blank=True, null=True)
+
+	no_leido = models.BooleanField(default=True, blank=False, db_index=True)
+	actor_content_type = models.ForeignKey(ContentType, 
+							related_name='notificar_actor', on_delete=models.CASCADE)
+
+	object_id_actor = models.PositiveIntegerField()
+	content_object = GenericForeignKey("actor_content_type", "object_id_actor")
+
+
+	verbo = models.CharField(max_length=255)
+	descripcion = models.TextField(blank=True, null=True)
+
+
