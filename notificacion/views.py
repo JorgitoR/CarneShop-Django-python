1bee4fbfc8ef64ce59006438a0acd4c6d08484ce
JorgitoR <jorgitouribe133@gmail.com>
2021-06-22 06:39:41 -0500

    Carneshop Django

--- notificacion/views.py
+++ notificacion/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import never_cache
+
+from django.views.generic import ListView
+
+
+from notificacion import settings
+from notificacion.settings import get_config
+
+from django.forms import model_to_dict
+from swapper import load_model
+
+from django.http import JsonResponse
+
+Notificacion = load_model('notificacion', 'notificacion')
+
+
+class NotificacionLista(ListView):
+	model = Notificacion
+
+
+
